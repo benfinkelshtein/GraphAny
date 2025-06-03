9d18be0cbdd89954b7de5b96401f208c4b096ddb
Ben Finkelshtein <ben.finkelshtein95@gmail.com>
2025-06-03 18:24:06 +0100

    upd

--- scripts_euler/mean_split.py
+++ scripts_euler/mean_split.py
@@ -28,7 +28,7 @@ commands = []
 for dataset in ["size1", "size3", "size5", "size7", "size9"]:
     for args in itertools.product(*get_grid(dataset)):
         command_str = ' '.join([f"{key}={value}" for key, value in zip(arg_names, args)])
-        cmd = f"--project GFM/Multi-GraphAny --command '{command_str}'"
+        cmd = f"--project GFM/Multi-GraphAny --command {command_str}"
         commands.append(cmd)
 
 print(f"Total commands generated: {len(commands)}")
