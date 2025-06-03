3f0511ede649a08b6563812c0aa578ca6600f597
Ben Finkelshtein <ben.finkelshtein95@gmail.com>
2025-06-03 18:16:04 +0100

    Update submit.sh

--- scripts_euler/submit.sh
+++ scripts_euler/submit.sh
@@ -20,7 +20,7 @@ conda activate graphany
 # Run the main.py script with the parameter set for this sbatch script
 while IFS= read -r line
 do
-  python -u /home/benfin/GraphAny/run_and_record.py \$line
+  python -u /home/benfin/GraphAny/run_and_record.py "\$line"
 done < /home/benfin/GraphAny/scripts_euler/grid/batch_${i}.txt
 EOF
 done
